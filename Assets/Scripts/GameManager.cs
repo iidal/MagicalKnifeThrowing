94e5a06de5083c60e2fb6a1c7b999ea1e3f3da5b
Iida <iida.lintuaho@gmail.com>
2020-01-07 00:28:17 +0200

    save system. saving and checking high score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,8 +9,17 @@ public class GameManager : MonoBehaviour
     public OrbManager orbManager;
 
     public GameObject endMenu;
+    [SerializeField]
+    TextMeshProUGUI endText;
+    [SerializeField]
+    TextMeshProUGUI highScoreTextMenu;
+
     public bool isGameOver = false;
     public int level = 1; //value for changing speeds and spawn delays as game progresses. player wont see this
+
+    //////
+    int highScore =0;
+    ///
 
     void Start()
     {
@@ -19,12 +29,26 @@ public class GameManager : MonoBehaviour
         else{
             instance = this;
         }
+        LoadGame();
     }
 
     public void GameOver(){
         isGameOver = true;
         orbManager.DestroyOrbs();
         endMenu.SetActive(true);
+
+        int score = PlayerManager.instance.score;
+
+        if(score > highScore){
+            highScore = score;
+            endText.text = "New high score!"+ score.ToString() + "! Good job b";
+            SaveLoad.SaveHighScore(score);
+
+        }
+        else{
+            endText.text = "You scored " + score.ToString();
+        }
+
     }
 
     public void StartGame(){
@@ -43,6 +67,11 @@ public class GameManager : MonoBehaviour
         EnvironmentController.instance.AdjustingBGMusic("Exit");
         yield return new WaitForSeconds(2f);
         EnvironmentController.instance.AdjustingBGMusic("ToMenu");
+    }
+
+    void LoadGame(){
+        highScore = SaveLoad.LoadHighScore();
+        highScoreTextMenu.text = "high score:\n" + highScore.ToString();
     }
 
 }
---
+++ Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+/*
+
+for saving game data, such as scores
+
+ */
+public static class SaveLoad
+{
+    
+    public static int highScore = 0; //scores for completed games
+
+    
+
+    #region high score
+    public static void SaveHighScore(int score)
+    {
+        highScore = score;
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/highscore.gd");
+        bf.Serialize(file, SaveLoad.highScore);
+        file.Close();
+
+    }
+    //loading data from file. rn only loads results from solo games
+    public static int LoadHighScore()
+    {
+        if (File.Exists(Application.persistentDataPath + "/highscore.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/highscore.gd", FileMode.Open);
+            SaveLoad.highScore = (int)bf.Deserialize(file);
+            file.Close();
+
+            return highScore;
+        }
+        return 0;
+    }
+    #endregion
+    // public static void DeleteFile(string toDelete)
+    // {
+    //     if(toDelete=="gameState"){
+    //         File.Delete(Application.persistentDataPath + "/gameStateSheet.gd");
+    //         File.Delete(Application.persistentDataPath + "/gameStateOther.gd");
+    //     }
+    // }
+}
