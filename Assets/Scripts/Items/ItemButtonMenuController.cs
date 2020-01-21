d6a808f7bf2d6585d5b213fcc9394fe61af4b760
Iida <iida.lintuaho@gmail.com>
2020-01-22 00:14:45 +0200

    starting start menu mechanics for items

---
+++ Assets/Scripts/Items/ItemButtonMenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemButtonMenuController : MonoBehaviour
+{
+    
+    [HideInInspector] public bool itemSelected = false;
+    Image itemIcon;
+    TextMeshProUGUI amountText;
+    GameObject selectionMark;
+    Image buttonBase;
+
+    void Start()
+    {
+        GameObject tempOb = transform.Find("ItemIcon").gameObject;
+        //item icon
+        itemIcon = tempOb.GetComponent<Image>();
+        //Amount indicator
+        tempOb = transform.Find("Amount").gameObject;
+        amountText = tempOb.GetComponentInChildren<TextMeshProUGUI>();
+        //item seleceted icon
+        selectionMark = transform.Find("CheckMark").gameObject;
+        selectionMark.SetActive(false);
+        //button base on start menu
+        buttonBase = GetComponent<Image>();
+        
+
+    }
+
+    public void ItemToggle(){
+        if(itemSelected){
+            itemSelected = false;
+            selectionMark.SetActive(false);
+        }
+        else{
+            itemSelected = true;
+            selectionMark.SetActive(true);
+        }
+    }
+
+    public void PopulateButton(Sprite icon, Color buttonColor){
+        itemIcon.sprite = icon;
+        buttonBase.color = buttonColor;
+    }
+}
---
+++ Assets/Scripts/Items/ItemManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    [SerializeField] ItemButtonMenuController[] buttons;
+    [SerializeField] SpellItem[] spellItems;
+
+    void Start()
+    {
+        PopulateButtons();
+    }
+    void PopulateButtons(){
+        for(int i = 0; i<buttons.Length; i++){
+            buttons[i].PopulateButton(spellItems[i].itemIcon, spellItems[i].itemBackgroundColor);
+
+        }
+    }
+    void OnGameStart(){
+        //
+        foreach(ItemButtonMenuController but in buttons){
+            //check waht items have been selected and use those in the game
+            
+        }
+    }
+
+}
