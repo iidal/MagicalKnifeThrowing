97daf66bcbdf51728421d6943fd2bd0d92c3c6d7
Iida <iida.lintuaho@gmail.com>
2020-10-08 22:19:17 +0300

    yay we have ads

---
+++ Assets/Scripts/ads/AdManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour, IUnityAdsListener
+{
+    string placement = "rewardedVideo";
+    string gameID = "3855559";
+   
+
+    void Start()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameID, true);
+        // while(!Advertisement.IsReady(placement))
+        //     yield return null;
+        // Advertisement.Show(placement);
+    }
+
+    public void ShowAd(string adType){
+
+        Advertisement.Show(adType);
+
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        //throw new System.NotImplementedException();
+        if(showResult == ShowResult.Finished){
+            Debug.Log("reward");
+            if(placementId == "CoinsEarned"){
+                Debug.Log("coins");
+                CoinManager.instance.AddCoins(25);
+            }
+            else if(placementId == "rewardedVideo"){
+                Debug.Log("revive");
+                GameManager.instance.WatchAd();
+
+            }
+        }
+        else if(showResult == ShowResult.Failed){
+            //o ou
+        }
+
+    }
+
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        //throw new System.NotImplementedException();
+    }
+    public void OnUnityAdsDidError(string message)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+}
---
+++ Assets/Scripts/ads/BannerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerManager : MonoBehaviour
+{
+    string placement = "MenuBanner";
+    string gameID = "3855559";
+   
+
+    IEnumerator Start()
+    {
+      
+        Advertisement.Initialize(gameID, true);
+        while(!Advertisement.IsReady(placement))
+            yield return null;
+        
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show(placement);
+    }
+
+
+    
+}
